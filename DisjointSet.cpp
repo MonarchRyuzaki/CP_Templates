b94733a6d92b0f0130cdbb4ad5ea42ca45bc2145
MonarchRyuzaki <114910769+MonarchRyuzaki@users.noreply.github.com>
2025-01-20 21:54:21 +0530

    Update DisjointSet.cpp. Added Code for number of components

--- DisjointSet.cpp
+++ DisjointSet.cpp
@@ -5,13 +5,15 @@ using namespace std;
 // Rectangle Sides and circles can also be taken as nodes (Any non node thing can be taken as nodes as long as we can come up with a relation how it interacts)
 class DisjointSet {
     vector<int> parent, size;
+    int n;
 public:
-    DisjointSet(int n){
-        parent.resize(n+1); size.resize(n+1);
-        for (int i=0; i<=n; i++){
+    DisjointSet(int n) {
+        parent.resize(n + 1); size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
             parent[i] = i;
             size[i] = 1;
         }
+        this->n = n;
     }
     int getLeader(int node) {
         if (node == parent[node]) return node;
@@ -25,7 +27,14 @@ public:
             size[ult_u] += size[ult_v];
         } else {
             parent[ult_u] = ult_v;
-            size[ult_v] += size[ult_u];            
+            size[ult_v] += size[ult_u];
         }
+    }
+    int getNumberOfComponents() {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            count += (parent[i] == i);
+        }
+        return count;
     }
 };
